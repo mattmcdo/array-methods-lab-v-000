32e9be91bf23fae0767ff23368ae66f7926adb63
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-06-21 01:23:23 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -19,7 +19,11 @@ def using_first(array)
 end
 
 def using_last(array)
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array.last
 end
 
 def using_size(array)
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array.length
 end
